e7e1d6fa52bf8a74df88e87eb07af28ba50073de
ochi <mune.ochi@gmail.com>
2022-12-22 16:13:31 +0900

    rewrite Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,47 +1,31 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        var number = 0
-        var max = -1
-        for (i in input.indices) {
-            if (input[i].isNotEmpty()) {
-                number += input[i].toInt()
-                if (i == input.size -1 && number > max) {
-                    max = number
-                }
+import java.io.File
 
-            } else {
-                if (number > max) max = number
-                number = 0
-            }
+fun main() {
+    fun parseInput(input: String) = input.split("\n\n").map { elf ->
+        elf.lines().map {
+            if (it.isNotEmpty()) it.toInt() else 0
         }
-        return max
     }
 
-    fun part2(input: List<String>): Int {
-        var number = 0
-        val totals = mutableListOf<Int>()
-        for (i in input.indices) {
-            if (input[i].isNotEmpty()) {
-                number += input[i].toInt()
-                if (i == input.size -1) {
-                    totals.add(number)
-                }
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+        return data.maxOf { it.sum() }
+    }
 
-            } else {
-                totals.add(number)
-                number = 0
-            }
-        }
-        totals.sortDescending()
-        return totals[0] + totals[1] + totals[2]
+    fun part2(input: String): Int {
+        val data = parseInput(input)
+        return data.map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = File("src/Day01_test.txt").readText()
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
     part1(input).println()
     part2(input).println()
 }
