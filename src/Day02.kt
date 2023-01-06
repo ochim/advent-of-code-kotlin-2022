b5d34a8f0f3000c8ad4afd7726dd93171baee76e
ochi <mune.ochi@gmail.com>
2023-01-06 22:30:31 +0900

    rewrite Day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,64 +1,43 @@
 fun main() {
+    val baseScores = mapOf(
+        'X' to 1, //Rock
+        'Y' to 2, //Paper
+        'Z' to 3  //Scissors
+    )
+
+    val roundScores = mapOf(
+        "A X" to 3,
+        "A Y" to 6,
+        "A Z" to 0,
+        "B X" to 0,
+        "B Y" to 3,
+        "B Z" to 6,
+        "C X" to 6,
+        "C Y" to 0,
+        "C Z" to 3,
+    )
+
     fun part1(input: List<String>): Int {
-        var score = 0
-        input.forEach {
-            val (opponent, you) = it.split(" ")
-            when (opponent) {
-                "A" -> {
-                    when (you) {
-                        "X" -> { score += 1 + 3 }
-                        "Y" -> { score += 2 + 6 }
-                        "Z" -> { score += 3 }
-                    }
-                }
-                "B" -> {
-                    when (you) {
-                        "X" -> { score += 1 }
-                        "Y" -> { score += 2 + 3 }
-                        "Z" -> { score += 3 + 6 }
-                    }
-                }
-                "C" -> {
-                    when (you) {
-                        "X" -> { score += 1 + 6 }
-                        "Y" -> { score += 2 }
-                        "Z" -> { score += 3 + 3 }
-                    }
-                }
-            }
+        return input.sumOf { str ->
+            val me = str[2]
+            baseScores[me]!! + roundScores[str]!!
         }
-        return score
     }
 
     fun part2(input: List<String>): Int {
-        var score = 0
-        input.forEach {
-            val (opponent, you) = it.split(" ")
-            when (opponent) {
-                "A" -> {
-                    when (you) {
-                        "X" -> { score += 3 }
-                        "Y" -> { score += 1 + 3 }
-                        "Z" -> { score += 2 + 6 }
-                    }
-                }
-                "B" -> {
-                    when (you) {
-                        "X" -> { score += 1 }
-                        "Y" -> { score += 2 + 3 }
-                        "Z" -> { score += 3 + 6 }
-                    }
-                }
-                "C" -> {
-                    when (you) {
-                        "X" -> { score += 2 }
-                        "Y" -> { score += 3 + 3 }
-                        "Z" -> { score += 1 + 6 }
-                    }
-                }
+        return input.sumOf { str ->
+            val op = str[0]
+
+            val roundScore: Int = when (str[2]) {
+                'X' -> 0
+                'Y' -> 3
+                'Z' -> 6
+                else -> throw IllegalArgumentException()
             }
+
+            val item = roundScores.firstNotNullOf { (key, value) -> if (key[0] == op && value == roundScore) key else null }
+            roundScore + baseScores[item[2]]!!
         }
-        return score
     }
 
     // test if implementation meets criteria from the description, like:
